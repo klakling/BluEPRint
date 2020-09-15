0a3d99e2cb6c4c16501bba732bed2e210ca344e5
Jens Krumsieck <jenso666@live.de>
2020-09-15 15:10:25 +0200

    Testing Linked Axis

---
+++ BluEPRint.Core/LinkedAxis.cs
@@ -0,0 +1,95 @@
+﻿using OxyPlot.Axes;
+using System;
+using System.Diagnostics;
+
+namespace BluEPRint.Core
+{
+    public class LinkedAxis : LinearAxis
+    {
+        /// <summary>
+        /// Parent Axis to Link to.
+        /// </summary>
+        private Axis _parent;
+
+        /// <summary>
+        /// Function that does conversion
+        /// </summary>
+        private Func<double, double> Converter { get; }
+
+        /// <summary>
+        /// function that does inverse conversion
+        /// </summary>
+        private Func<double, double> InverseConverter { get; }
+
+        /// <summary>
+        /// variable to prevent endless looping
+        /// </summary>
+        private bool _isConverting;
+
+        public LinkedAxis(Axis parent, Func<double, double> converter, Func<double, double> inverseConverter)
+        {
+            _parent = parent;
+            Converter = converter;
+            InverseConverter = inverseConverter;
+            StartPosition = 1;
+            EndPosition = 0;
+            Maximum = Converter(_parent.AbsoluteMinimum);
+            Minimum = Converter(_parent.AbsoluteMaximum);
+            AbsoluteMinimum = Converter(_parent.AbsoluteMaximum);
+            AbsoluteMaximum = Converter(_parent.AbsoluteMinimum);
+            _parent.AxisChanged += ParentChanged;
+            AxisChanged += MeChanged;
+        }
+
+        /// <summary>
+        /// Transforms the specified coordinate to screen coordinates.
+        /// <see cref="Axis.Transform(double)"/>
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public override double Transform(double x)
+        {
+            var parentValue = InverseConverter(x);
+            return _parent.Transform(parentValue);
+        }
+
+        /// <summary>
+        /// Inverse transforms the specified screen coordinate. This method can only be used with non-polar coordinate systems.
+        /// <see cref="Axis.InverseTransform(double)"/>
+        /// </summary>
+        /// <param name="sx"></param>
+        /// <returns></returns>
+        public override double InverseTransform(double sx)
+        {
+            var parentValue = InverseConverter(sx);
+            return _parent.InverseTransform(parentValue);
+        }
+
+        /// <summary>
+        /// Adjust Zoom when parent changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void ParentChanged(object sender, AxisChangedEventArgs eventArgs)
+        {
+            //prevent endless loop
+            if (_isConverting) return;
+            _isConverting = true;
+            Zoom(Converter(_parent.AbsoluteMinimum), Converter(_parent.AbsoluteMaximum));
+            _isConverting = false;
+        }
+
+        /// <summary>
+        /// Adjust Zoom when Axis changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void MeChanged(object sender, AxisChangedEventArgs eventArgs)
+        {
+            if (_isConverting) return;
+            _isConverting = true;
+            _parent.Zoom(InverseConverter(ActualMinimum), InverseConverter(ActualMaximum));
+            _isConverting = false;
+        }
+    }
+}
--- BluEPRint/MainWindow.xaml.cs
+++ BluEPRint/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BluEPRint.Core;
+using BluEPRint.Core.Spectrum;
+using ChemSharp.Spectrum;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BluEPRint
 {
@@ -23,6 +17,36 @@ namespace BluEPRint
         public MainWindow()
         {
             InitializeComponent();
+            var path =
+                @"C:\Users\jenso\PowerFolders\Forschung\EPR\PyOMeIPCoCl\5_55k\Spectrum6";
+            var spec = SpectrumFactory.Create<PlottableEPRSpectrum>(path + ".par", path + ".spc");
+            var model = new PlotModel();
+            var x = new LinearAxis()
+            {
+                Position = AxisPosition.Bottom,
+                AbsoluteMaximum = spec.Data.Max(s => s.X),
+                AbsoluteMinimum = spec.Data.Min(s => s.X)
+            };
+            var g = new LinkedAxis(x, spec.BtoG, spec.GtoB)
+            {
+                Position = AxisPosition.Top
+            };
+            var y = new LinearAxis()
+            {
+                Position = AxisPosition.Left,
+                TickStyle = TickStyle.None,
+                IsAxisVisible = false
+            };
+            model.Axes.Add(y);
+            model.Axes.Add(x);
+            model.Axes.Add(g);
+
+            var series = new LineSeries()
+            {
+                ItemsSource = spec.Data.Select(s => new DataPoint(s.X, s.Y))
+            };
+            model.Series.Add(series);
+            TestPlotView.Model = model;
         }
     }
 }
